6e8f77d89588cbd583f5fd1b29ff0f7680c1a0be
Janis Cardenas <hope.sandoval58@gmail.com>
2024-01-20 21:51:13 +0800

    Primer commit NavBar y Footer

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+const HomePage = () => {
+  return (
+    <>
+      <header className="row col">
+        <h1>Intro</h1>
+      </header>
+      <main className="row">
+        <article className="col">
+          <p>Yes Please is a dinner & dive bar that is known for it’s strong signature cocktail line, chill-house music, delicious food and the 90’s-vibe Instagram-worthy interiors.</p>
+        </article>
+      </main>
+    </>
+  );
+};
+
+export default HomePage;
